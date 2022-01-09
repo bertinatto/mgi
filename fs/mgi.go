31c3d37783a2385761c59f70415a8e1923064e5a
Fabio <fabio@fabio>
2022-01-09 14:56:02 -0300

    Small refactoring to make things clearer

--- fs/mgi.go
+++ fs/mgi.go
@@ -164,75 +164,76 @@ func (m *MGIService) writeTree() (string, error) {
 }
 
 // TODO: move this to object.go
-type TreeEntry struct {
-	Mode uint32
-	Path string
-	Sha1 [20]byte
+type treeEntry struct {
+	mode uint32
+	path string
+	sha1 [20]byte
 }
 
+// writeSubTree writes a tree object for the given path. It may be called recursively.
+// The subTree parameter must end with a slash ("/") or it can be an emtpy string (to represent the current directory
 func (m *MGIService) writeSubTree(subTree string, entries []*mgi.IndexEntry) (string, []byte, error) {
 	if subTree == "." {
 		subTree = ""
 	}
+	if len(subTree) > 0 && !strings.HasSuffix(subTree, "/") {
+		subTree += "/"
+	}
 
-	children := make(map[string]*TreeEntry)
+	// First of all, we are going to figure out the entries for our tree object
+	children := make(map[string]*treeEntry)
 	for _, indexEntry := range entries {
-		// TODO: replace Split with Dir, so that the trailing / is gone
-		entryDir, _ := filepath.Split(indexEntry.Path)
+		entryDir, entryFile := filepath.Split(indexEntry.Path)
 		if entryDir == subTree {
-			// The entry is a direct child of the subTree, so add it to our object
-			e := &TreeEntry{
-				Mode: indexEntry.Mode,
-				Path: filepath.Base(indexEntry.Path),
-				Sha1: indexEntry.Sha1,
+			// The entry is both a file and a direct child of the subTree, so add it to our tree object listing
+			e := &treeEntry{
+				mode: indexEntry.Mode,
+				// path: filepath.Base(indexEntry.Path),
+				path: entryFile,
+				sha1: indexEntry.Sha1,
 			}
-			children[e.Path] = e
+			children[e.path] = e
 
-		} else {
-			// We are only interested in direct childs
-			if strings.HasPrefix(entryDir, subTree) {
-				// Hey, dir is a child of subTree, but is it a direct child?
-				directChild := strings.TrimPrefix(entryDir, subTree) // a/b/c, a/ -> b/c
-				dirs := strings.Split(directChild, "/")
+		} else if strings.HasPrefix(entryDir, subTree) {
+			// The entry is somewhere under subTree (not necessarily a direct child, though)
+			relEntry := strings.TrimPrefix(entryDir, subTree) // a/b/c, a/ -> b/c
+			directChild := strings.Split(relEntry, "/")[0]
 
-				directChild = dirs[0]
-				if directChild == "" {
-					continue
-				}
-				if _, ok := children[directChild]; ok {
-					continue
-				}
-
-				nextDirPath := filepath.Join(subTree, directChild) + "/"
-				_, sumBytes, err := m.writeSubTree(nextDirPath, entries)
-				if err != nil {
-					return "", nil, err
-				}
-
-				e := &TreeEntry{
-					Mode: 040000,
-					Path: directChild,
-				}
-				copy(e.Sha1[:], sumBytes)
-				children[e.Path] = e
+			if _, ok := children[directChild]; ok {
+				continue
 			}
+
+			// Before we can create the tree object for subTree, we need to create an object for this sub-directory
+			nextDirPath := filepath.Join(subTree, directChild) + string(os.PathSeparator)
+			_, sumBytes, err := m.writeSubTree(nextDirPath, entries)
+			if err != nil {
+				return "", nil, err
+			}
+
+			e := &treeEntry{
+				mode: 040000,
+				path: directChild,
+			}
+			copy(e.sha1[:], sumBytes)
+			children[e.path] = e
 		}
 	}
 
+	// Now the we know the entries, write the tree object
 	if len(children) > 0 {
-		lines := make([]*TreeEntry, 0, len(children))
+		lines := make([]*treeEntry, 0, len(children))
 		for _, v := range children {
 			lines = append(lines, v)
 		}
 
 		// Entries should be sorted in acending order
 		sort.Slice(lines, func(i, j int) bool {
-			return lines[i].Path < lines[j].Path
+			return lines[i].path < lines[j].path
 		})
 
 		b := new(bytes.Buffer)
 		for _, e := range lines {
-			b.WriteString(fmt.Sprintf("%o %s\x00%s", e.Mode, e.Path, e.Sha1))
+			b.WriteString(fmt.Sprintf("%o %s\x00%s", e.mode, e.path, e.sha1))
 		}
 
 		sum, sumBytes, err := m.obj.Store(mgi.TreeType, b.Bytes())
